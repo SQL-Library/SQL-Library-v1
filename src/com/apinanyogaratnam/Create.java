db68dc19fa316eefaa2c0721b88c14517eeb66dd
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-15 23:20:43 -0400

    created Secrets file for user to get and set username, password and url for sql server

--- src/com/apinanyogaratnam/Create.java
+++ src/com/apinanyogaratnam/Create.java
@@ -1,5 +1,22 @@
 package com.apinanyogaratnam;
 
-public class Create {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class Create {
+    Secrets secrets = new Secrets();
+    public void createNewTable(String query) {
+        try {
+            Connection connection = DriverManager.getConnection(secrets.getUrl(), secrets.getUsername(), secrets.getPassword());
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+
+            // close connection to db
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/com/apinanyogaratnam/Secrets.java
@@ -0,0 +1,31 @@
+package com.apinanyogaratnam;
+
+public class Secrets {
+    private String username;
+    private String password;
+    private String url;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+}
