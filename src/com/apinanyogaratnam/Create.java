9a05344463690ff855c737d67bbeac1e40441d78
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-16 11:57:12 -0400

    simplifying and making reusable code

--- src/com/apinanyogaratnam/Create.java
+++ src/com/apinanyogaratnam/Create.java
@@ -1,25 +1,10 @@
 package com.apinanyogaratnam;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class Create extends SQL{
 
     public void createNewTable(String query) {
-        try {
-            Connection connection = DriverManager.getConnection(secrets.getUrl(), secrets.getUsername(), secrets.getPassword());
-            Statement statement = connection.createStatement();
-
-            // create new table, add more parameters to method for attributes about the table
-            statement.executeUpdate(query);
-
-            // close connection to db
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // create new table, add more parameters to method for attributes about the table
+        SQLCommand(query);
     }
 
     public void createNewObject(String query) {
